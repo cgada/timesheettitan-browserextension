14073332646ada3c37f5476b3be71e3cb813f635
Michael Rose <ekimsc1094@sympatico.ca>
2015-09-22 20:33:40 -0400

    Create basic stop/start task mechanism
    - Add assets for inactive/active icons

--- src/titan-extension.js
+++ src/titan-extension.js
@@ -1,4 +1,31 @@
+function getActiveTasks(callback) {
+  callback([]);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-	// This is the code that gets called when the extension icon is clicked.
-	// Start from here.
+  var startTaskButton = document.getElementById('start_task');
+  var stopTaskButton = document.getElementById('stop_task');
+
+  function setupNoActiveTasksState() {
+    chrome.browserAction.setIcon({ path: 'assets/titan-inactive.png' });
+    stopTaskButton.style.display = 'none';
+    startTaskButton.style.display = 'block';
+  }
+
+  function setupActiveTasksState() {
+    chrome.browserAction.setIcon({ path: 'assets/titan-active.png' });
+    stopTaskButton.style.display = 'block';
+    startTaskButton.style.display = 'none';
+  }
+
+  getActiveTasks(function(tasks) {
+    if(tasks.length === 0) {
+      setupNoActiveTasksState();
+    } else {
+      setupActiveTasksState();
+    }
+  });
+
+  startTaskButton.addEventListener('click', setupActiveTasksState);
+  stopTaskButton.addEventListener('click', setupNoActiveTasksState);
 });
